9738ece0be45ef289a85b99dd8f9b896c7727e45
jlsoaresramos <jlramossoares@outlook.com.br>
2024-09-30 15:41:02 -0300

    Add font Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				Inter: ["Inter", "sans-serif"],
+			},
 			backgroundImage: {
 				background:
 					"linear-gradient(to bottom left, #22162B 25%, #833BE7 100%)",
