827828358d67619258252df0ee7906ac6a49752c
jlsoaresramos <jlramossoares@outlook.com.br>
2024-09-30 15:24:03 -0300

    Add design variables and set background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				background:
+					"linear-gradient(to bottom left, #22162B 25%, #833BE7 100%)",
+				"button-background":
+					"linear-gradient(to bottom left, #833BE7, #5CC8FF )",
+			},
+			colors: {
+				primary: {
+					purple: "#833BE7",
+					red: "#F15156",
+					"dark-purple": "#22162B",
+					black: "#2E2532",
+					blue: "#5CC8FF",
+				},
+			},
+		},
 	},
 	plugins: [],
 };
