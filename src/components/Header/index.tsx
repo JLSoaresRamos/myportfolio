296ae24ed8816721097ad0625dac1643784fa87a
jlsoaresramos <jlramossoares@outlook.com.br>
2024-10-03 20:24:36 -0300

    Fixed menumobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,8 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Logo from "../common/Logo";
-
 import MobileFloatNav from "./MobileFloatNav";
 import Nav from "./Nav";
 
@@ -14,14 +13,24 @@ export default function Header() {
 		setIsOpen(!isOpen);
 	}
 
+	useEffect(() => {
+		function handleResize() {
+			if (window.innerWidth > 768) {
+				setIsOpen(false);
+			}
+		}
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
 		<header className="flex items-center justify-between pt-4">
 			<Logo />
-			<button
-				className="md:hidden"
-				onClick={() => toggle()}
-				aria-label="Abrir menu"
-			>
+			<button className="md:hidden" onClick={toggle} aria-label="Abrir menu">
 				{isOpen ? (
 					<IoMdClose className="w-6 h-10 cursor-pointer text-2xl" />
 				) : (
